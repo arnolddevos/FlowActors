80423581cca7bca3f25543241e02db3bb33ab376
Arnold deVos <adv@langdale.com.au>
2013-10-24 11:40:19 +1100

    comment the API

--- src/main/scala/au/com/langdale/async/Flow.scala
+++ src/main/scala/au/com/langdale/async/Flow.scala
@@ -4,6 +4,7 @@ import language.higherKinds
 
 trait Flow {
 
+  /** External view of an input port for wiring */
   type InputPort[-Message] <: InputOps[Message]
 
   trait InputOps[-Message] {
@@ -11,6 +12,7 @@ trait Flow {
     def buffer(n: Int): Unit
   }
 
+  /** External view of an output port for wiring */
   type OutputPort[+Message] <: OutputOps[Message]
 
   trait OutputOps[+Message] { 
@@ -18,22 +20,12 @@ trait Flow {
     def disconnect: Unit
   }
 
-  trait Actor { 
-    protected val self = site(this)
-    def input[Message]( buffer: Int = 1) = self.input[Message](buffer)
-    def output[Message]() = self.output[Message]()
-    def stop = self.stop
-    def error = self.error
-    def start = self.inject(act())
-
-    type Action = self.Action
-    protected def act(): Action
-  }
-
+  /** A site at which messages are processed. */
   type Site <: SiteOps
 
   trait SiteOps {
 
+    /** A continuation that can be dispatched at this site */
     type Action 
     type InputAction <: Action with ActionCombinator
 
@@ -41,30 +33,55 @@ trait Flow {
       def orElse( alt: InputAction): InputAction
     }
 
+    /** Internal view of an input port for reacting to incoming messages */
     trait InputReactor[+Message] {
       def apply( step: Message => Action ): InputAction
       def react( step: Message => Action ) = apply(step)
     }
 
+    /** Internal view of an output port for sending outgoing messages */
     trait OutputReactor[-Message] {
       def apply( m: Message)( step: => Action ): Action 
     }
 
-    // methods to create Ports
+    /** Create an input port */
     def input[Message]( buffer: Int = 1): InputReactor[Message] with InputPort[Message]
+
+    /** Create an output port */
     def output[Message](): OutputReactor[Message] with OutputPort[Message]
     
-    // ways to stop: the error Port and stop action
+    /** The type of object used in error reporting */
     type Reference
+
+    /** The error output port for this site */
     def error: OutputReactor[(Reference, Throwable)] with OutputPort[(Reference, Throwable)]
+
+    /** An empty continuation */
     def stop: Action
 
-    // method to run this actor 
+    /** Inject a continuation to be dispatched at this site */ 
     def inject(step: => Action, instances: Int = 1): Unit
   }
 
-  def site[R](ref: R): Site { type Reference = R }
+  /** Create a Site */
   def site(): Site { type Reference = Site }
+
+  /** Create a Site with a reference for error reporting */
+  def site[R](ref: R): Site { type Reference = R }
+
+  /** A base trait for a simple actor wrapping a Site */
+  trait Actor { 
+    protected val self = site(this)
+
+    type Action = self.Action
+    def input[Message]( buffer: Int = 1) = self.input[Message](buffer)
+    def output[Message]() = self.output[Message]()
+    def stop = self.stop
+    def error = self.error
+
+    def start = self.inject(act())
+    protected def act(): Action
+  }
 }
 
 object Flow extends Flow with FlowImpl with FlowExecutor.ForkJoin with FlowTrace.Graphviz
