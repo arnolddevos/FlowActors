eade12756a7549b57cf1b9bb26074c98cba291b6
Arnold deVos <adv@langdale.com.au>
2013-12-20 15:31:41 +1100

    wrap Process in an Activation  activation object and thread that though in place of Process

--- src/main/scala/au/com/langdale/async/FlowImpl.scala
+++ src/main/scala/au/com/langdale/async/FlowImpl.scala
@@ -8,6 +8,8 @@ trait FlowImpl extends Flow with Primitives with Queueing { this: Trace with Exe
 
   def createSite = new Site
 
+  case class Activation(process: Process)  
+
   final class Site extends SiteOps with PrimitiveActor { site =>
 
     private[FlowImpl] val inputs = new StateMap with Queueing {
@@ -26,24 +28,24 @@ trait FlowImpl extends Flow with Primitives with Queueing { this: Trace with Exe
         }
       }
 
-    private[FlowImpl] def runStep( process: Process, step: => Action )(implicit t: Task) = spawn { implicit t => 
+    private[FlowImpl] def runStep( activation: Activation, step: => Action )(implicit t: Task) = spawn { implicit t => 
       val a = try { 
         trace(site, "=>", "Step")
         step 
       }
       catch { 
         case NonFatal(e) => 
-          trace("error", site, process, "uncaught exception", e)
-          output(errors, (this, process, e), 0)(stop)
+          trace("error", site, activation.process, "uncaught exception", e)
+          output(errors, (this, activation.process, e), 0)(stop)
       }
       
-      enqueue(implicit t => a.dispatch(this, process))
+      enqueue(implicit t => a.dispatch(this, activation))
     } 
 
     /** inject an action into the site */
     def run(process: Process, instances: Int) = request { implicit t => 
       trace(site, "=>", "Run")
-      for( i <- 1 to instances) runStep( process, process.action )
+      for( i <- 1 to instances) runStep( Activation(process), process.action )
     }
 
     /** change buffering depth */  
@@ -86,7 +88,7 @@ trait FlowImpl extends Flow with Primitives with Queueing { this: Trace with Exe
   def stop: Action = Stop()
 
   sealed trait Action {
-    private[FlowImpl] def dispatch(site: Site, process: Process)(implicit t: Task): Unit
+    private[FlowImpl] def dispatch(site: Site, activation: Activation)(implicit t: Task): Unit
   }
   
   sealed trait InputAction extends Action with ActionCombinator {
@@ -94,15 +96,15 @@ trait FlowImpl extends Flow with Primitives with Queueing { this: Trace with Exe
   }
 
   private class OutputAction[Message]( label: OutputPort[Message], n: Int, m: Message)( step: => Action ) extends Action {
-    private[FlowImpl] def dispatch(site: Site, process: Process)(implicit t: Task) = { import site._
-      outputs(label, n) { outputs.transition(implicit t => { runStep(process, step); m })}
+    private[FlowImpl] def dispatch(site: Site, activation: Activation)(implicit t: Task) = { import site._
+      outputs(label, n) { outputs.transition(implicit t => { runStep(activation, step); m })}
     }
   }
 
   private trait BasicInputAction extends InputAction {
-    private[FlowImpl] def dispatch(site: Site, process: Process)(implicit t: Task) = dispatchLater(site, process, Cancellation { t => () })
-    def dispatchLater(site: Site, process: Process, cancel: Cancellation)(implicit t: Task): Cancellation
-    def dispatchNow(site: Site, process: Process)(implicit t: Task): Boolean
+    private[FlowImpl] def dispatch(site: Site, activation: Activation)(implicit t: Task) = dispatchLater(site, activation, Cancellation { t => () })
+    def dispatchLater(site: Site, activation: Activation, cancel: Cancellation)(implicit t: Task): Cancellation
+    def dispatchNow(site: Site, activation: Activation)(implicit t: Task): Boolean
   }
 
   case class Cancellation( f: Task => Unit ) {
@@ -111,20 +113,20 @@ trait FlowImpl extends Flow with Primitives with Queueing { this: Trace with Exe
 
   private class SingleInputAction[Message]( label: InputPort[Message])( step: Message => Action ) extends BasicInputAction {
       
-    def dispatchLater(site: Site, process: Process, cancel: Cancellation)(implicit t: Task) = { import site._
+    def dispatchLater(site: Site, activation: Activation, cancel: Cancellation)(implicit t: Task) = { import site._
       val id = new CancelRef
       inputs(label) { 
         inputs.transition[Message](id){ implicit t => m =>
-          cancel.run; runStep( process, step(m))
+          cancel.run; runStep( activation, step(m))
         }
       }
       Cancellation { implicit t => inputs(label) { inputs.cancel(id) }}
     }
 
-    def dispatchNow(site: Site, process: Process)(implicit t: Task): Boolean = { import site._
+    def dispatchNow(site: Site, activation: Activation)(implicit t: Task): Boolean = { import site._
       inputs.run(label) { 
         inputs.transitionMaybe[Message]{ implicit t => m =>
-          runStep( process, step(m))
+          runStep( activation, step(m))
         }
       }
     }
@@ -139,12 +141,12 @@ trait FlowImpl extends Flow with Primitives with Queueing { this: Trace with Exe
   }
 
   private class TimerAction(delay: Long)(step: => Action) extends BasicInputAction {
-    def dispatchLater(site: Site, process: Process, cancel: Cancellation)(implicit t: Task) = {
+    def dispatchLater(site: Site, activation: Activation, cancel: Cancellation)(implicit t: Task) = {
       @volatile var cancelled = false
-      val event = timer.after(delay) { site.enqueue { implicit t => if(!cancelled) { cancel.run; site.runStep( process, step)}}}
+      val event = timer.after(delay) { site.enqueue { implicit t => if(!cancelled) { cancel.run; site.runStep( activation, step)}}}
       Cancellation { t => cancelled = true; event.cancel }
     }
-    def dispatchNow(site: Site, process: Process)(implicit t: Task) = false
+    def dispatchNow(site: Site, activation: Activation)(implicit t: Task) = false
   }
   
   private case class Alternatives(a1: InputAction, a2: InputAction) extends InputAction {
@@ -157,27 +159,27 @@ trait FlowImpl extends Flow with Primitives with Queueing { this: Trace with Exe
       gather(this)
     }
     
-    private[FlowImpl] def dispatch(site: Site, process: Process)(implicit t: Task): Unit = {
+    private[FlowImpl] def dispatch(site: Site, activation: Activation)(implicit t: Task): Unit = {
       val actions = toList
-      val done = actions.exists(_.dispatchNow(site, process))
+      val done = actions.exists(_.dispatchNow(site, activation))
       if( ! done ) {
         var cs: List[Cancellation] = List(Cancellation( t => sys.error("dispatch called outside enqueue")))
         def cancelAll = Cancellation { implicit t => for( c <- cs ) { c.run }}
-        cs = actions.map( _.dispatchLater(site, process, cancelAll))
+        cs = actions.map( _.dispatchLater(site, activation, cancelAll))
       }
     }
   }
 
   private class Control( step: (Site, Process) => Action ) extends Action {
-    private[FlowImpl] def dispatch(site: Site, process: Process)(implicit t: Task) = { 
-      site.runStep(process, step(site, process))
+    private[FlowImpl] def dispatch(site: Site, activation: Activation)(implicit t: Task) = { 
+      site.runStep(activation, step(site, activation.process))
     }
   }
   
   private case class Stop() extends Action {
-    private[FlowImpl] def dispatch(site: Site, process: Process)(implicit t: Task) { 
-      trace(site, process, "=>", "Stop")
-      for( following <- process.followedBy )
+    private[FlowImpl] def dispatch(site: Site, activation: Activation)(implicit t: Task) { 
+      trace(site, activation.process, "=>", "Stop")
+      for( following <- activation.process.followedBy )
         site.run(following)
     }
   }
